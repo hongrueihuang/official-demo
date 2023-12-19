75b3a371284ca90f25ba28555065df04f9955373
rayhuang <ray.huang@stockfeel.com.tw>
2023-12-19 17:04:48 +0800

    Updates

---
+++ static/index.08e50320.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.c27b6911.js";import{x as _,q as c}from"./swiper-vue.f129d564.js";const s={},t={class:"product-wrapper"};function o(r,a){return c(),_("div",t,"This is products page")}const p=e(s,[["render",o],["__scopeId","data-v-93dfac7b"]]);export{p as default};
---
+++ static/logo.d2044297.js
@@ -0,0 +1,1 @@
+import"./entry.9ffc6f6f.js";const s=""+globalThis.__publicAssetsURL("img/logo.png");export{s as _};
