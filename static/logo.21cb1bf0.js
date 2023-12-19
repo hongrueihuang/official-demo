e2126c5628f9c5463a670db4fc21208d34cde34f
rayhuang <ray.huang@stockfeel.com.tw>
2023-12-19 17:40:38 +0800

    Updates

--- static/logo.21cb1bf0.js
+++
@@ -1,1 +0,0 @@
-import"./entry.72643069.js";const s=""+globalThis.__publicAssetsURL("img/logo.png");export{s as _};
---
+++ static/logo.aa022ee2.js
@@ -0,0 +1,1 @@
+import"./entry.727353ae.js";const s=""+globalThis.__publicAssetsURL("img/logo.png");export{s as _};
